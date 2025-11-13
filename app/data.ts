e199cbb7e83deb63a10abbf58e0ec12b199b8840
Nate <oneblurryeye@gmail.com>
2025-11-13 14:55:51 -0500

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -26,43 +26,43 @@ export const projectBlurbs: { [key: string]: string } = {
 
 export const PROJECTS: Project[] = [
   {
-    name: 'FSU Tech Connect: A P2P Marketplace Study',
+    name: 'FSU Tech Connect',
     description:
       'A mixed-methods study to uncover student needs for a peer-to-peer tech marketplace, translating research insights into actionable feature recommendations.',
     link: '/p2p-marketplace-research',
-    image: '/project1-bg.png',
+    image: '/project1-tn.png',
     id: 'project1',
   },
   {
-    name: 'Pivoting for Paws: A UX Research Case Study',
+    name: 'Pivoting for Paws',
     description:
       "A case study in challenging assumptions. How initial heuristics were corrected by user data, leading to targeted, high-impact recommendations for a non-profit.",
     link: '/its-meow-or-never',
-    image: '/project2-prototype-hero.png',
+    image: '/project2-tn.png',
     id: 'project2',
   },
   {
-    name: "The Gamer's Voice: A Reddit Data Analysis",
+    name: "The Handheld Gamer's Voice",
     description:
       'A data-driven analysis using Python to scrape and analyze community discussions on Reddit, identifying key user pain points and feature opportunities for handheld gaming.',
     link: '/handheld-analysis',
-    image: '/project3-bg.png',
+    image: '/project3-tn.png',
     id: 'project3',
   },
   {
-    name: 'The Wellness Report: Sleep vs. Stress',
+    name: 'Student Wellness Report',
     description:
       'A quantitative analysis of student survey data to find correlations between sleep habits and stress. Cleaned, visualized, and presented findings using Excel and Looker Studio.',
     link: '/student-wellness-study',
-    image: '/project4-lookerstudio.png',
+    image: '/project4-tn.png',
     id: 'project4',
   },
   {
-    name: 'Sleeper vs. [Competitor]: A Usability Benchmark',
+    name: 'Sleeper vs. Competitors',
     description:
       'A comparative usability study analyzing how Sleeper’s core social features (chat, media sharing) perform against key competitors. This project identifies key friction points and strategic opportunities to enhance user engagement and solidify Sleeper’s mission to connect friends over sports.',
     link: '/sleeper-research',
-    image: '/placeholder.png',
+    image: '/project5-tn.png',
     id: 'project5',
   },
 ]
