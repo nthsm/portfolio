516bd19e235860ff3693338b7e250c843cd9436d
Nate <oneblurryeye@gmail.com>
2025-11-13 10:13:00 -0500

    Delete mdx.d.ts

--- mdx.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.mdx' {
-  export const title: string
-  export const description: string
-  export const image: string
-  export const date: string
-
-  export default function MDXContent(props: any): JSX.Element
-}
